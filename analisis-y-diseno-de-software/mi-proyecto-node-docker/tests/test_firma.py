642799027c8dc04f3474a81dd5fa71ecc516a951
vichooo <vsilva@usm.cl>
2026-06-03 20:43:31 -0400

    arreglo de el test_01
    se arregló el test_01_verificar_pin_valido segun lo dicho en la pauta del hito 3

--- analisis-y-diseno-de-software/mi-proyecto-node-docker/tests/test_firma.py
+++ analisis-y-diseno-de-software/mi-proyecto-node-docker/tests/test_firma.py
@@ -14,19 +14,27 @@ class TestAuthCodeAPI(unittest.TestCase):
     def tearDownClass(cls):
         print("--- Pruebas finalizadas ---")
 
-    # Endpoint: POST /api/auth-code/verify
     def test_01_verificar_pin_valido(self):
-        url = f"{self.base_url}/verify"
-        payload = {
+        url = f"{self.base_url}/verify"        
+        payload_exitoso = {
             "rut": self.rut_prueba,
             "code": "123456"
         }
         
-        response = requests.post(url, json=payload, headers=self.headers)
-        data = response.json()
+        response_exitoso = requests.post(url, json=payload_exitoso, headers=self.headers)
+        data_exitoso = response_exitoso.json()
         
-        self.assertEqual(response.status_code, 200, "Debe retornar 200 OK")
-        self.assertTrue(data.get("ok"), "El status 'ok' debe ser true")
+        self.assertEqual(response_exitoso.status_code, 200, "Debe retornar 200 OK")
+        self.assertTrue(data_exitoso.get("ok"), "El status 'ok' debe ser true")
+        payload_falso = {
+            "rut": "99999999-9", # RUT que no existe
+            "code": "123456"
+        }
+        
+        response_falso = requests.post(url, json=payload_falso, headers=self.headers)
+        data_falso = response_falso.json()        
+        self.assertNotEqual(response_falso.status_code, 200, "No debe retornar 200 si el RUT no está en la BD")
+        self.assertFalse(data_falso.get("ok", True), "El status 'ok' debe ser false o no existir si el RUT es inválido")
 
     def test_02_verificar_pin_invalido_letras(self):
         url = f"{self.base_url}/verify"
@@ -39,7 +47,7 @@ class TestAuthCodeAPI(unittest.TestCase):
         data = response.json()
         
         self.assertEqual(response.status_code, 400, "Debe retornar 400 Bad Request")
-        self.assertFalse(data.get("valid"), "El status 'valid' debe ser false")
+        self.assertFalse(data.get("ok", True), "El status 'ok' debe ser false")
         self.assertEqual(data.get("message"), "El código debe tener exactamente 6 dígitos")
 
 if __name__ == '__main__':
